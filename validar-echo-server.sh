69eb85d89c2a2c5471590f9dfad8f82c0e35dcda
Carolina Racedo <cracedo@fi.uba.ar>
2026-03-14 00:39:11 -0300

    fix: replace == with =

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -6,7 +6,7 @@ TEST_MESSAGE="8ACE00"
 
 RESPONSE=$(docker run --rm --network $NETWORK busybox sh -c "echo '$TEST_MESSAGE' | nc $SERVER_HOST $SERVER_PORT")
 
-if [ "$RESPONSE" == "$TEST_MESSAGE" ]; then
+if [ "$RESPONSE" = "$TEST_MESSAGE" ]; then
     echo "action: test_echo_server | result: success"
 else
     echo "action: test_echo_server | result: fail"
